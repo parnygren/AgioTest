0abbe6aa89795060ac77469135d27e6f42870ecc
Pär Nygren <par.a.nygren@gmail.com>
2017-11-25 09:20:59 +0100

    Add project files.

---
+++ AgioTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AgioTest.Startup))]
+namespace AgioTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
