f829a0a987831a1da4e377a1001cfb7331c8afdc
Pär Nygren <par.a.nygren@gmail.com>
2017-11-25 10:27:41 +0100

    Create Add an Employee form

---
+++ AgioTest/Controllers/EmployeesController.cs
@@ -0,0 +1,20 @@
+﻿using AgioTest.Models;
+using System.Web.Mvc;
+
+namespace AgioTest.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
